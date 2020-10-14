68fd297da8c876b2ad1925374dc5eb1f5b93ea8d
Yuliya Pakidzka <ypakidzka@ibagroup.eu>
2020-10-15 01:29:23 +0300

    fix by comments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
+/* eslint-disable no-unused-vars */
 "use strict";
 
-const avatars = [
-  `img/avatars/user01.png`,
-  `img/avatars/user02.png`,
-  `img/avatars/user03.png`,
-  `img/avatars/user04.png`,
-  `img/avatars/user05.png`,
-  `img/avatars/user06.png`,
-  `img/avatars/user07.png`,
-  `img/avatars/user08.png`,
-];
+const ADS_NUM = 8;
 
 const titles = [`title01`, `title02`, `title03`];
 
@@ -43,50 +35,53 @@ const photos = [
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
 ];
 
-const randomInteger = function (min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+const getRandomInteger = function (min, max) {
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
 const getRandomFromArray = function (arr) {
-  let rand = Math.floor(Math.random() * arr.length);
+  const rand = getRandomInteger(0, arr.length - 1);
   return arr[rand];
 };
 
-const getRandomElements = function (arr, length) {
-  let result = [];
-
-  for (let i = 0; i < length; i++) {
-    let randomElement = getRandomFromArray(arr);
-    result.push(randomElement);
+const shuffle = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-
-  return result;
+  return array;
 };
 
-const createArray = (length) => {
-  let array = [];
+const getRandomArrayElements = function (arr) {
+  const shuffledArray = shuffle(arr);
+  const rand = getRandomInteger(1, shuffledArray.length - 1);
+  return shuffledArray.slice(0, rand);
+};
+
+const createAdsData = (length) => {
+  const array = [];
   for (let index = 1; index <= length; index++) {
-    let object = {
+    const object = {
       author: {
-        avatar: getRandomFromArray(avatars),
+        avatar: `img/avatars/user0` + index + `.png`,
       },
       offer: {
         title: getRandomFromArray(titles),
-        address: `{{location.x}}, {{location.y}}`,
-        price: randomInteger(100, 1000),
+        address: `600, 350`,
+        price: getRandomInteger(100, 1000),
         type: getRandomFromArray(types),
-        rooms: randomInteger(1, 5),
-        guests: randomInteger(1, 10),
+        rooms: getRandomInteger(1, 5),
+        guests: getRandomInteger(1, 10),
         checkin: getRandomFromArray(times),
         checkout: getRandomFromArray(times),
-        features: getRandomElements(features, randomInteger(1, features.length)),
+        features: getRandomArrayElements(features),
         description: getRandomFromArray(descriptions),
-        photos: getRandomElements(photos, randomInteger(1, photos.length)),
+        photos: getRandomArrayElements(photos),
       },
       location: {
-        x: randomInteger(130, 630),
-        y: randomInteger(130, 630),
+        x: getRandomInteger(130, 630),
+        y: getRandomInteger(130, 630),
       },
     };
     array.push(object);
@@ -94,34 +89,25 @@ const createArray = (length) => {
   return array;
 };
 
-let makeElement = function (tagName, className) {
-  let element = document.createElement(tagName);
-  if (className) {
-    element.classList.add(className);
-  }
-  return element;
-};
+const template = document.querySelector(`#pin`).content;
 
-let createElement = function (elementData) {
-  let listItem = makeElement(`button`, `map__pin`);
-  listItem.setAttribute(`style`, `left: ` + elementData.location.x + `px; top: ` + elementData.location.y + `px`);
+const fragment = document.createDocumentFragment();
 
-  let img = makeElement(`img`);
+const data = createAdsData(ADS_NUM);
+
+data.forEach((elementData) => {
+  let element = template.cloneNode(true);
+
+  let button = element.querySelector(`.map__pin`);
+  button.setAttribute(`style`, `left: ` + elementData.location.x + `px; top: ` + elementData.location.y + `px`);
+
+  let img = element.querySelector(`img`);
   img.src = elementData.author.avatar;
   img.alt = elementData.offer.title;
-  img.setAttribute(`height`, 40);
-  img.setAttribute(`width`, 40);
-  img.setAttribute(`draggable`, false);
 
-  listItem.appendChild(img);
-
-  return listItem;
-};
-
-let mapPins = document.querySelector(`.map__pins`);
-
-createArray(8).forEach((element) => {
-  let htmlElement = createElement(element);
-  mapPins.appendChild(htmlElement);
-
+  fragment.appendChild(element);
 });
+
+const mapPins = document.querySelector(`.map__pins`);
+
+mapPins.appendChild(fragment);
